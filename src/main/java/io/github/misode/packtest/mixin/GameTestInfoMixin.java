9b88522c0cb1084306bb92ef89efb9eb0195f6e1
Misode <misoloo64@gmail.com>
2024-01-08 07:05:55 +0100

    Clear dummies in test on succeed

--- src/main/java/io/github/misode/packtest/mixin/GameTestInfoMixin.java
+++ src/main/java/io/github/misode/packtest/mixin/GameTestInfoMixin.java
@@ -1,9 +1,15 @@
 package io.github.misode.packtest.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import io.github.misode.packtest.ChatListener;
 import io.github.misode.packtest.PackTestInfo;
+import io.github.misode.packtest.dummy.Dummy;
 import net.minecraft.gametest.framework.GameTestInfo;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,9 +18,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 /**
  * Adds chat listener field and accessors. Removes the listener when finishing.
  * Prevents crash when test has already started.
+ * Clears dummies after succeeding.
  */
 @Mixin(GameTestInfo.class)
-public class GameTestInfoMixin implements PackTestInfo {
+public abstract class GameTestInfoMixin implements PackTestInfo {
+
+    @Shadow
+    public abstract ServerLevel getLevel();
 
     @Unique
     private ChatListener chatListener;
@@ -37,5 +47,11 @@ public class GameTestInfoMixin implements PackTestInfo {
     @Inject(method = "finish", at = @At("HEAD"))
     private void finish(CallbackInfo ci) {
         this.chatListener.stop();
+    }
+
+    @Inject(method = "succeed", at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V", shift = At.Shift.AFTER))
+    private void succeed(CallbackInfo ci, @Local(ordinal = 0) AABB aabb) {
+        this.getLevel().getEntitiesOfClass(Dummy.class, aabb.inflate(1))
+                .forEach(dummy -> dummy.leave(Component.literal("Test succeeded")));
     }
 }
