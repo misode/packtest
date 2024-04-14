9d583b7ca6baa875deb4ba346b6467254a2d725e
Misode <misoloo64@gmail.com>
2024-04-14 02:16:41 +0200

    Update to 1.20.5-pre1

--- src/main/java/io/github/misode/packtest/mixin/TagLoaderMixin.java
+++ src/main/java/io/github/misode/packtest/mixin/TagLoaderMixin.java
@@ -2,10 +2,8 @@ package io.github.misode.packtest.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.mojang.serialization.DataResult;
 import io.github.misode.packtest.LoadDiagnostics;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagFile;
 import net.minecraft.tags.TagLoader;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
@@ -13,22 +11,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.function.Consumer;
-
 /**
  * Catch tag errors and removes stacktrace.
- * Removes the duplicate data result error.
  */
 @Mixin(TagLoader.class)
 public class TagLoaderMixin {
     @Shadow
     @Final
     private static Logger LOGGER;
-
-    @WrapOperation(method = "load", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/DataResult;getOrThrow(ZLjava/util/function/Consumer;)Ljava/lang/Object;", remap = false))
-    private static Object removeDuplicateError(DataResult<Object> dataResult, boolean allowPartial, Consumer<String> onError, Operation<TagFile> original) {
-        return dataResult.getOrThrow(allowPartial, (error) -> {});
-    }
 
     @WrapOperation(method = "load", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;[Ljava/lang/Object;)V", remap = false))
     private static void catchTagError(Logger logger, String message, Object[] args, Operation<Void> original) {
